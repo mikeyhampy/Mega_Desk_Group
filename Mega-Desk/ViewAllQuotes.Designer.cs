7494bb3fbda7c21bf8a8adb0a446df9f9d381dd5
cvanfleet <dvanfleet.clayton@gmail.com>
2022-10-11 21:35:50 -0600

    Edited the ViewAllQuotes form, and added placeholder data to the generate_quotes method and viewDetailsBtn method. JSON data will need to flow into these methods

--- Mega-Desk/ViewAllQuotes.Designer.cs
+++ Mega-Desk/ViewAllQuotes.Designer.cs
@@ -29,6 +29,9 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.quotesBox = new System.Windows.Forms.ListBox();
+            this.viewDetailsBtn = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -41,21 +44,59 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(165, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(181, 31);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Quote History";
+            // 
+            // quotesBox
+            // 
+            this.quotesBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.quotesBox.FormattingEnabled = true;
+            this.quotesBox.ItemHeight = 24;
+            this.quotesBox.Location = new System.Drawing.Point(84, 72);
+            this.quotesBox.Name = "quotesBox";
+            this.quotesBox.Size = new System.Drawing.Size(328, 148);
+            this.quotesBox.TabIndex = 2;
+            // 
+            // viewDetailsBtn
+            // 
+            this.viewDetailsBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.viewDetailsBtn.Location = new System.Drawing.Point(190, 236);
+            this.viewDetailsBtn.Name = "viewDetailsBtn";
+            this.viewDetailsBtn.Size = new System.Drawing.Size(111, 43);
+            this.viewDetailsBtn.TabIndex = 3;
+            this.viewDetailsBtn.Text = "View Details";
+            this.viewDetailsBtn.UseVisualStyleBackColor = true;
+            this.viewDetailsBtn.Click += new System.EventHandler(this.viewDetailsBtn_Click);
+            // 
             // ViewAllQuotes
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(496, 291);
+            this.Controls.Add(this.viewDetailsBtn);
+            this.Controls.Add(this.quotesBox);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "ViewAllQuotes";
             this.Text = "ViewAllQuotes";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ViewAllQuotes_FormClosed);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox quotesBox;
+        private System.Windows.Forms.Button viewDetailsBtn;
     }
 }
--- Mega-Desk/ViewAllQuotes.cs
+++ Mega-Desk/ViewAllQuotes.cs
@@ -15,6 +15,7 @@ namespace Mega_Desk_Hampton
         public ViewAllQuotes()
         {
             InitializeComponent();
+            generate_quotes();
         }
 
         private void Button1_Click(object sender, EventArgs e)
@@ -28,6 +29,35 @@ namespace Mega_Desk_Hampton
         {
             var mainMenu = (MainMenu)Tag;
             mainMenu.Show();
+        }
+
+        /**
+         * generate_quotes method
+         * Purpose: To display quote information from JSON file 
+         */
+        public void generate_quotes()
+        {
+
+            //This is just a placeholder for when we get the JSON data. 
+            // I was thinking initially we could just show the quote NAME, AMOUNT AND DATE and then
+            // the viewDetailsbtn button could pull up the DisplayQuote form with all the additional information
+            // for the selected AddQuote object (the viewDetailsbtn click event is found just below this method).
+            // please let me know if this was confusing, or if you have a better idea
+            for (int i = 1; i < 4; i++)
+            {
+                quotesBox.Items.Add($"Quote {i}");
+            }
+            
+        }
+        /**
+         * viewDetailsBtn on click event
+         * Purpose: to allow the user to see all the quote details from the selected quote
+         */
+        private void viewDetailsBtn_Click(object sender, EventArgs e)
+        {
+            var select = quotesBox.SelectedItem.ToString();
+
+            MessageBox.Show("This is a placeholder, we can instead pull up the DisplayQuote form to show all additional information", select);
         }
     }
 }
